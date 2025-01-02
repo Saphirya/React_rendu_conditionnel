1e3dba6ffa1e344866c1923adc7fe5951f48d9e5
Florian <floriangeorges1992@gmail.com>
2025-01-02 08:06:50 +0100

    add ternaire

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Articles from "./components/Articles.jsx";
 function App() {
   return (
     <div className="d-flex flex-row justify-content-center align-items-center p-20">
-      <Articles />
+      <Articles displayArticle={true} />
     </div>
   );
 }
