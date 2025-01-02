8d7d0c10464be3a70700bfaf236d2babac3dc972
Florian <floriangeorges1992@gmail.com>
2025-01-02 07:50:32 +0100

    add components articles.jsx

---
+++ src/components/Articles.jsx
@@ -0,0 +1,18 @@
+function Articles() {
+  return (
+    <div style={{ width: "700px" }}>
+      <h1 className="mb-20">Liste des articles</h1>
+      <div className="card p-20">
+        <h2 className="mb-10">Titre de l&apos;article</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+          architecto culpa veniam ut excepturi ex dolorem? Possimus sit laborum
+          quibusdam exercitationem! Earum est quidem corporis id? Distinctio
+          tenetur amet voluptates!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Articles;
