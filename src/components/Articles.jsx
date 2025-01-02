c47718171fabdb4c08543b9e964097b7ea20b618
Florian <floriangeorges1992@gmail.com>
2025-01-02 16:55:44 +0100

    add unik key

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,27 +1,32 @@
 function Articles() {
   const articles = [
     {
-      title: "Titre de l'article",
+      title: "Titre de l'article 1",
       content: "contenu 1",
+      published: true,
     },
     {
-      title: "Titre de l'article",
+      title: "Titre de l'article 2",
       content: "contenu 2",
+      published: true,
     },
     {
-      title: "Titre de l'article",
+      title: "Titre de l'article 3",
       content: "contenu 3",
+      published: false,
     },
   ];
   return (
     <div style={{ width: "700px" }}>
       <h1 className="mb-20">Liste des articles</h1>
-      {articles.map((article) => (
-        <div key={article.title} className="mb-20">
-          <h2>{article.title}</h2>
-          <p>{article.content}</p>
-        </div>
-      ))}
+      {articles
+        .filter((a) => a.published)
+        .map((article) => (
+          <div key={article.title} className="mb-20">
+            <h2>{article.title}</h2>
+            <p>{article.content}</p>
+          </div>
+        ))}
     </div>
   );
 }
