69b303dc7f6fd3ea791fe6bc2a4fdb2f376fc566
Florian <floriangeorges1992@gmail.com>
2025-01-02 08:38:40 +0100

    add .map

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,14 +1,27 @@
-import Article from "./Article.jsx";
-function Articles({ displayArticle }) {
-  let content = <div>Aucun article</div>;
-  if (displayArticle) {
-    content = <Article />;
-  }
-
+function Articles() {
+  const articles = [
+    {
+      title: "Titre de l'article",
+      content: "contenu 1",
+    },
+    {
+      title: "Titre de l'article",
+      content: "contenu 2",
+    },
+    {
+      title: "Titre de l'article",
+      content: "contenu 3",
+    },
+  ];
   return (
     <div style={{ width: "700px" }}>
       <h1 className="mb-20">Liste des articles</h1>
-      {content}
+      {articles.map((article) => (
+        <div key={article.title} className="mb-20">
+          <h2>{article.title}</h2>
+          <p>{article.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
